4b2198baaab18824eb8858854d97d5e4d8574a77
Alex Ford <alextford1@gmail.com>
2022-08-16 14:26:38 +0100

    remove print

--- app/spotify.py
+++ app/spotify.py
@@ -35,7 +35,6 @@ class Spotify:
     def get_stored_tokens():
         print(SPOTIFY_ACCESS_TOKEN_JSON)
         print([f for f in os.listdir(ROOT_DIR)])
-        print([f for f in os.listdir(os.path.join(ROOT_DIR, 'app'))])
         with open(SPOTIFY_ACCESS_TOKEN_JSON, 'r') as f:
             tokens = json.load(f)
             print(tokens)
