7a62e6c5c73c25448a4f28d339f0043f15da2c63
Alex Ford <alextford1@gmail.com>
2022-08-16 12:10:23 +0100

    add command

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
 from app import create_app
+from app.spotify import scheduler_check_and_execute
 
 app = create_app()
+
+
+@app.cli.command()
+def run_scheduler():
+    scheduler_check_and_execute()
 
 
 if __name__ == '__main__':
